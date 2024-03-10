4696d27b956dc2323536f4673ccd8f34392c60a0
Fernando <jesusfgm45@gmail.com>
2024-03-10 08:23:58 -0600

    Recuperar informacion de un posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,28 @@
 const crearUsuario = (req, res) => {
+    console.log(req.body);
+    const {name, email, password} = req.body;
+    if(name.length < 5){
+        return res.status(400).json({
+            ok: false,
+            msg: 'El nombre debe contener 5 caracteres o más',
+        })
+    }
     res.json({
         ok: true,
-        msg: 'register'
+        msg: 'register',
+        name,
+        email,
+        password
     })
 }
 
 const loginUsuario = (req, res) => {
+    const {email, password} = req.body;
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        email,
+        password
     })
 }
 
--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 //Un middleware no es una funcion que se ejecuta en el momento en el que alguien hace una peticion a mi servidor
 app.use(express.static('public'));
 
+//Lectura y parseo del body
+app.use(express.json());
+
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
 //TODO: CRUD: Eventos
