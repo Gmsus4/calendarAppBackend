3899582ecf8e6b1dba5f0f58bb59d4d904f4f18f
Fernando <jesusfgm45@gmail.com>
2024-03-10 11:35:29 -0600

    Login del usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,6 +21,7 @@ const crearUsuario = async(req, res) => {
         usuario.password = bcrypt.hashSync( password, salt );
 
         await usuario.save();
+        //Generar JWT
     
         res.status(201).json({
             ok: true,
@@ -36,15 +37,43 @@ const crearUsuario = async(req, res) => {
     }
 }
 
-const loginUsuario = (req, res) => {
+const loginUsuario = async(req, res) => {
     const {email, password} = req.body;
-    
-    res.status(201).json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+
+    try {
+        const usuario = await Usuario.findOne({email: email});
+
+        if(!usuario){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email',
+            })
+        }
+
+        //Confirmar los passwords
+        const validPassword = bcrypt.compareSync( password, usuario.password );
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto',
+            })
+        }
+
+        // Generar nuestro JWt
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador',
+        })
+    }
 }
 
 const revalidarToken = (req, res) => {
