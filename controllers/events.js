5fb67852b97945f098714cfbf10e3579f9ee0dbe
Fernando <jesusfgm45@gmail.com>
2024-03-10 20:15:18 -0600

    Actualizar un evento

--- controllers/events.js
+++ controllers/events.js
@@ -37,18 +37,44 @@ const crearEvento = async(req, res) => {
     }
 }
 
-const actualizarEvento = (req, res) => {
+const actualizarEvento = async(req, res) => {
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
     try {
+        const evento = await Evento.findById(eventoId);
+
+        if(!evento){
+            res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            })
+        }
+
+        if(evento.user.toString() !== uid){
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            })
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, {new: true} );
         res.status(201).json({
             ok: true,
-            msg: 'actualizarEvento'
-        })  
+            evento: eventoActualizado
+        });
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
             ok: false,
             msg: 'Por favor hable con el administrador',
-        })
+        });
     }
 }
 
